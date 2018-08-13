4ea865a99c2a0909ae6b40571df954874539d69d
Dakon <wiwi59410@gmail.com>
2018-08-13 10:34:55 +0200

    stop warning

--- src/controller/ChoisirQuizzController.java
+++ src/controller/ChoisirQuizzController.java
@@ -1,11 +1,11 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.Session;
 
 /**
  * Servlet implementation class ChoisirQuizzController
